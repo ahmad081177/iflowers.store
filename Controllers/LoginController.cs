9cfe99673921debc0c8df5180ef352a2282ed23a
Ahmad Agbaria <ahmad.agbaria@siemens.com>
2022-09-10 10:59:45 +0300

    initial promote: Login page (user, password, email) + logout

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using FlowerStoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlowerStoreMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DoLogin(UserTable user)
+        {
+            using (FlowersDBEntities db = new FlowersDBEntities())
+            {
+                var check = db.UserTables.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+                if (check == null)
+                {
+                    user.LoginErrorMessage = "Wrong username or password.";
+                    return View("Index", user);
+                }
+                //user exists
+                Session["user.Id"] = user.Id;
+                Session["user.Email"] = user.Email;
+                Session["user.Name"] = user.UserName;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
